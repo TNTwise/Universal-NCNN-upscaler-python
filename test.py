48e4868db36db45a99dc3d11ee68cb9e3189bf79
TNTwise <tntwise3@gmail.com>
2024-02-25 18:28:17 -0600

    should work now for every model

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from upscale_ncnn_py import UPSCALE
 
 from PIL import Image
 
-upscale1 = UPSCALE(gpuid=0,model=4)
+upscale1 = UPSCALE(gpuid=0,model=15)
 with Image.open("input.png") as image:
     image = upscale1.process_pil(image)
     image.save("output.jpg", quality=95)
