245ef5f28f100a16e5040ab707f22a50dfb9bc0b
TNTwise <tntwise3@gmail.com>
2024-03-23 13:26:00 -0500

    fix naming

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from realesrgan_ncnn_py import Realesrgan
 from upscale_ncnn_py import UPSCALE
-
+from realcugan_ncnn_py import Realcugan
 from PIL import Image
+#13
+upscale1 = UPSCALE(gpuid=0,model=21,num_threads=2)
 
-upscale1 = UPSCALE(gpuid=0,model=15,num_threads=2)
-with Image.open("input.png") as image:
-    image = upscale1.process_pil(image)
-    image.save("output.jpg", quality=95)
+with Image.open("Pasted image.png") as image:
+    
+        image = upscale1.process_pil(image)
+
+image.save("output1.png", quality=95)
